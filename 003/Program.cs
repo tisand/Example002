e1d39f92adb281bbe6388234660cff62c6c07143
Bekbolsun Alkozhoev <alkozhoev.b@gmail.com>
2023-01-02 15:41:48 +0500

    Третье задание

---
+++ 003/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {31, 22, 15, 56, 78, 63, 41, 37};
+
+int n = array.Length;
+int find = 41;
+int index = 0;
+
+while (index < n) {
+    if (array[index] == find)
+        {
+            Console.WriteLine(index);
+            break;
+        }
+    index++;
+}
